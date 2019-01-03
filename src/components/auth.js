45938844d735f3ef5d1e763adc824ab5284a60c8
Gil Greenberg <gilgreenberg@gmail.com>
2019-01-02 21:10:14 -0500

    Add simple logic to check for token and shop param and add to cookies
    TODO: Add to state. Will need to move this logic to Layout component so we don't loose global app state

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { replace } from 'gatsby'
+import Cookies from 'universal-cookie'
 
 const AuthWrapper = ({children}) => {
-    const isAuthenticated = false // get authToken from cookie
+    const cookies = new Cookies();
+    const urlParams = new URLSearchParams(window.location.search);
+    const shop = urlParams.get('shop');
+    const token = urlParams.get('token');
 
-    if (!isAuthenticated) {
-        replace(`/install`)
-        return <></>
-    } else {
-        return children
+    if (shop && token) {
+
+        cookies.set('shop', shop, { path: '/' });
+        cookies.set('token', token, { path: '/' });
+
+        // this.setState({ 
+        //     shop,
+        //     token
+        // });
     }
 
+    if (cookies.get('shop') && cookies.get('token')) {
+        return children
+    } else {
+        replace(`/install`)
+        return <></>
+    }
 }
 
 export default AuthWrapper
