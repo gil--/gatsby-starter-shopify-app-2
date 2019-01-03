eb92bcc96cc3ea9064789ff036c4e5b2c58d81ce
Gil Greenberg <gilgreenberg@gmail.com>
2019-01-02 23:19:32 -0500

    Add middleware Shopify OAuth logic

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,10 +1,44 @@
+var ShopifyToken = require('shopify-token');
+require('dotenv').config();
+
+var {
+    SHOPIFY_APP_API_KEY,
+    SHOPIFY_APP_SHARED_SECRET,
+} = process.env;
+
 exports.handler = function (event, context, callback) {
-    const { shop } = event.queryStringParameters;
+    var { shop } = event.queryStringParameters;
+
+    if (!shop) {
+        callback(null, {
+            statusCode: 302,
+            headers: {
+                Location: '/install',
+                'Cache-Control': 'no-cache' // Disable caching of this response
+            },
+            body: '' // return body for local dev
+        })
+    }
+
+    var shopifyToken = new ShopifyToken({
+        "redirectUri": `https://${event.headers.host}/.netlify/functions/callback`,
+        "sharedSecret": SHOPIFY_APP_SHARED_SECRET,
+        "apiKey": SHOPIFY_APP_API_KEY,
+        "scopes": "read_orders,write_orders"
+    });
+    shopifyToken.shop = shop.replace('.myshopify.com', '');
+
+    var nonce = shopifyToken.generateNonce();
+    var uri = shopifyToken.generateAuthUrl(shopifyToken.shop, undefined, nonce);
+
+    console.log('Redirecting to ' + uri);
+
     callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            text: 'Auth function',
-            shop,
-        }),
+        statusCode: 302,
+        headers: {
+            Location: uri,
+            'Cache-Control': 'no-cache' // Disable caching of this response
+        },
+        body: '' // return body for local dev
     })
 }
--- src/functions/callback.js
+++ src/functions/callback.js
@@ -1,8 +1,50 @@
+var ShopifyToken = require('shopify-token');
+require('dotenv').config();
+
+var {
+    SHOPIFY_APP_API_KEY,
+    SHOPIFY_APP_SHARED_SECRET,
+} = process.env;
+
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            text: 'Hello, World! I’m some text sent from a Lambda function.',
-        }),
-    })
+    var { code, shop } = event.queryStringParameters;
+
+    var shopifyToken = new ShopifyToken({
+        "redirectUri": `https://${event.headers.host}/.netlify/functions/callback`,
+        "sharedSecret": SHOPIFY_APP_SHARED_SECRET,
+        "apiKey": SHOPIFY_APP_API_KEY,
+        "scopes": "read_orders,write_orders"
+    });
+    
+    if (shopifyToken.verifyHmac(event.queryStringParameters)) {
+        // Get permanent access token that will be used in the future to make API calls
+        shopifyToken.getAccessToken(shop, code).then((token) => {
+
+            console.log(`Generated token ${token} for shop ${shop}`);
+            //createApplicationCharge(req.query.shop, token, res);
+            callback(null, {
+                statusCode: 302,
+                headers: {
+                    Location: `/?token=${token}&shop=${shop}`,
+                    'Cache-Control': 'no-cache' // Disable caching of this response
+                },
+                body: '' // return body for local dev
+            })
+
+        }).catch((err) => {
+            //console.error(err.stack);
+
+            callback(null, {
+                statusCode: 500,
+                body: 'Oops, something went wrong',
+            });
+        });
+    } else {
+        console.error('Error validating hmac');
+        
+        callback(null, {
+            statusCode: 500,
+            body: 'Error validating hmac',
+        });
+    }
 }
