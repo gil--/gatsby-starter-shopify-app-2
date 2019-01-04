38bc93731b63f3d4a781e377abb8d4bb4f01aa51
Gil Greenberg <gilgreenberg@gmail.com>
2019-01-03 20:04:49 -0500

    Wrap all logic in window check - no need to run during Gatsby SSR

--- src/components/auth.js
+++ src/components/auth.js
@@ -3,37 +3,37 @@ import { replace } from 'gatsby'
 import Cookies from 'universal-cookie'
 
 const AuthWrapper = ({children}) => {
-    const cookies = new Cookies();
-    const queryParams = window.location.search;
-    const urlParams = new URLSearchParams(queryParams);
-    let shop = null;
-    let token = null;
-
     if (typeof window !== 'undefined' && queryParams) {
+        const cookies = new Cookies();
+        const queryParams = window.location.search;
+        const urlParams = new URLSearchParams(queryParams);
+        let shop = null;
+        let token = null;
+
         shop = urlParams.get('shop');
         token = urlParams.get('token');
-    }
 
-    if (shop && token) {
-        cookies.set('shop', shop, { path: '/' });
-        cookies.set('token', token, { path: '/' });
+        if (shop && token) {
+            cookies.set('shop', shop, { path: '/' });
+            cookies.set('token', token, { path: '/' });
 
-        // this.setState({ 
-        //     shop,
-        //     token
-        // });
-    }
+            // this.setState({ 
+            //     shop,
+            //     token
+            // });
+        }
 
-    if (cookies.get('shop') && cookies.get('token')) {
-        return children
-    } else {
-        if (typeof window !== 'undefined' && window) {
+        if (cookies.get('shop') && cookies.get('token')) {
+            return children
+        } else {
             if (urlParams.get('hmac')) {
                 replace(`/.netlify/functions/reauth${queryParams}`)
             } else {
                 replace(`/install`)
             }
+            return <></>
         }
+    } else {
         return <></>
     }
 }
